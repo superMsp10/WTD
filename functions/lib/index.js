cfa3852fffe1b78317ae8b085ba62cf603de2932
superMsp10 <pandeymahan34@gmail.com>
2018-07-13 12:28:32 -0400

    Scraping V1

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -5,7 +5,7 @@ const isURL_1 = require("validator/lib/isURL");
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 var Xray = require('x-ray');
-var x = Xray().timeout(300).driver('request').delay(100);
+var x = Xray();
 // const corsFn = require('cors')("Access-Control-Allow-Origin");
 const db = admin.firestore();
 // firebase deploy --only functions
@@ -22,61 +22,47 @@ exports.helloWorld = functions.https.onRequest((req, res) => {
 // scrape({json: {"scrapeURL": "https://www.google.com/"}})
 exports.scrape = functions.https.onCall((data, context) => {
     const url = data.scrapeURL;
-    console.log("Scrapeing...");
+    // console.log("Scrapeing...");
     if (!url === undefined && !isURL_1.default.isURL(url)) {
         // Throwing an HttpsError so that the client gets the error details. 
         throw new functions.https.HttpsError('invalid-argument', 'The url must be valid');
     }
     else {
-        console.log(url);
-        // const html = "<body><h2>Pear</h2></body>";
-        return x(url, 'body', 'h2')
+        // console.log(url);
+        return x(url, 'title')
             .then((title) => {
-            console.log("title: " + title);
+            // console.log("title: " + title);
             return { scrape: title };
         })
             .catch(function (err) {
             throw new functions.https.HttpsError('not-found', 'The url could not be fetched', err);
         });
-        // return x(url, 'body', 'title')
-        //     .then((title) => {
-        //         console.log("title: " + title);
-        //         return { scrape: title };
-        //     })
-        //     .catch(function (err) {
-        //         throw new functions.https.HttpsError('not-found', 'The url could not be fetched', err);
-        //     });
-        // return x(html, 'body', 'h2')
-        //     .then((res) => {
-        //         console.log("header" + res);
-        //         return { data: res };
-        //     });
     }
 });
-exports.addMessage = functions.https.onCall((data, context) => {
-    // Message text passed from the client.
-    const text = data.text;
-    // Checking attribute.
-    if (!(typeof text === 'string') || text.length === 0) {
-        // Throwing an HttpsError so that the client gets the error details.
-        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
-            'one arguments "text" containing the message text to add.');
-    }
-    // Authentication / user information is automatically added to the request.
-    // const uid = context.auth.uid;
-    // const name = context.auth.token.name || null;
-    // const picture = context.auth.token.picture || null;
-    // const email = context.auth.token.email || null;
-    // returning result.
-    return { text: text + " || " };
-});
-exports.saveData = functions.https.onRequest((request, response) => {
-    var scrapes = db.collection('Scrapes');
-    var setAlan = scrapes.add({
-        'Name': 'Test',
-        'URL': 'TEST.com',
-        'Description': 'Hello'
-    });
-    response.send("Hello from Firebase!");
-});
+// export const addMessage = functions.https.onCall((data, context) => {
+//     // Message text passed from the client.
+//     const text = data.text;
+//     // Checking attribute.
+//     if (!(typeof text === 'string') || text.length === 0) {
+//         // Throwing an HttpsError so that the client gets the error details.
+//         throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+//             'one arguments "text" containing the message text to add.');
+//     }
+//     // Authentication / user information is automatically added to the request.
+//     // const uid = context.auth.uid;
+//     // const name = context.auth.token.name || null;
+//     // const picture = context.auth.token.picture || null;
+//     // const email = context.auth.token.email || null;
+//     // returning result.
+//     return { text: text + " || " };
+// });
+// export const saveData = functions.https.onRequest((request, response) => {
+//     var scrapes = db.collection('Scrapes');
+//     var setAlan = scrapes.add({
+//         'Name': 'Test',
+//         'URL': 'TEST.com',
+//         'Description': 'Hello'
+//     });
+//     response.send("Hello from Firebase!");
+// });
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,7 +5,7 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 var Xray = require('x-ray');
-var x = Xray().timeout(300).driver('request').delay(100);
+var x = Xray();
 // const corsFn = require('cors')("Access-Control-Allow-Origin");
 
 const db = admin.firestore();
@@ -26,72 +26,55 @@ export const helloWorld = functions.https.onRequest((req, res) => {
 export const scrape = functions.https.onCall((data, context) => {
 
     const url = data.scrapeURL;
-    console.log("Scrapeing...");
+    // console.log("Scrapeing...");
 
     if (!url === undefined && !isURL.isURL(url)) {
         // Throwing an HttpsError so that the client gets the error details. 
         throw new functions.https.HttpsError('invalid-argument', 'The url must be valid');
     } else {
-        console.log(url);
-        // const html = "<body><h2>Pear</h2></body>";
-        return x(url, 'body', 'h2')
-        .then((title) => {
-            console.log("title: " + title);
-            return { scrape: title };
-        })
-        .catch(function (err) {
-            throw new functions.https.HttpsError('not-found', 'The url could not be fetched', err);
-        });
-        
-        // return x(url, 'body', 'title')
-        //     .then((title) => {
-        //         console.log("title: " + title);
-        //         return { scrape: title };
-        //     })
-        //     .catch(function (err) {
-        //         throw new functions.https.HttpsError('not-found', 'The url could not be fetched', err);
-        //     });
-
-
-        // return x(html, 'body', 'h2')
-        //     .then((res) => {
-        //         console.log("header" + res);
-        //         return { data: res };
-        //     });
+        // console.log(url);
+        return x(url, 'title')
+            .then((title) => {
+                // console.log("title: " + title);
+                return { scrape: title };
+            })
+            .catch(function (err) {
+                throw new functions.https.HttpsError('not-found', 'The url could not be fetched', err);
+            });
     }
 });
 
-export const addMessage = functions.https.onCall((data, context) => {
-    // Message text passed from the client.
-    const text = data.text;
+// export const addMessage = functions.https.onCall((data, context) => {
+//     // Message text passed from the client.
+//     const text = data.text;
 
-    // Checking attribute.
-    if (!(typeof text === 'string') || text.length === 0) {
-        // Throwing an HttpsError so that the client gets the error details.
-        throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
-            'one arguments "text" containing the message text to add.');
-    }
+//     // Checking attribute.
+//     if (!(typeof text === 'string') || text.length === 0) {
+//         // Throwing an HttpsError so that the client gets the error details.
+//         throw new functions.https.HttpsError('invalid-argument', 'The function must be called with ' +
+//             'one arguments "text" containing the message text to add.');
+//     }
 
-    // Authentication / user information is automatically added to the request.
-    // const uid = context.auth.uid;
-    // const name = context.auth.token.name || null;
-    // const picture = context.auth.token.picture || null;
-    // const email = context.auth.token.email || null;
+//     // Authentication / user information is automatically added to the request.
+//     // const uid = context.auth.uid;
+//     // const name = context.auth.token.name || null;
+//     // const picture = context.auth.token.picture || null;
+//     // const email = context.auth.token.email || null;
 
-    // returning result.
-    return { text: text + " || " };
+//     // returning result.
+//     return { text: text + " || " };
 
-});
+// });
 
-export const saveData = functions.https.onRequest((request, response) => {
-    var scrapes = db.collection('Scrapes');
+// export const saveData = functions.https.onRequest((request, response) => {
+//     var scrapes = db.collection('Scrapes');
 
-    var setAlan = scrapes.add({
-        'Name': 'Test',
-        'URL': 'TEST.com',
-        'Description': 'Hello'
-    });
+//     var setAlan = scrapes.add({
+//         'Name': 'Test',
+//         'URL': 'TEST.com',
+//         'Description': 'Hello'
+//     });
 
-    response.send("Hello from Firebase!");
+//     response.send("Hello from Firebase!");
 
-});
+// });
